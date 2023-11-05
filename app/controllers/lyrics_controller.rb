94f81867b22ceccbe0b67c9d3ee207500e223539
Serena Green <serenafgreen@gmail.com>
2023-11-04 21:40:00 -0700

    adds method to call all

--- app/controllers/lyrics_controller.rb
+++ app/controllers/lyrics_controller.rb
@@ -3,4 +3,14 @@ class LyricsController < ApplicationController
     lyric = Lyric.first
     render json: Lyric.first
   end
+
+  def second_song
+    lyric = Lyric.second
+    render json: Lyric.second
+  end
+
+  def all_songs
+    lyric = Lyric.all
+    render json: Lyric.all
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,5 @@
 Rails.application.routes.draw do
   get "/one_lyric", controller: "lyrics", action: "one_song"
+  get "/second_lyric", controller: "lyrics", action: "second_song"
+  get "/all_lyrics", controller: "lyrics", action: "all_songs"
 end
