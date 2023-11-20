04f0f7567635d693beea0b0ad5cdca2670403152
“Serena” <serenafgreen@gmail.com>
2023-11-19 20:29:26 -0800

    adds patch, update, and delete requests

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,10 @@
 Rails.application.routes.draw do
-  get "/one_lyric", controller: "lyrics", action: "one_song"
-  get "/second_lyric", controller: "lyrics", action: "second_song"
-  get "/all_lyrics", controller: "lyrics", action: "all_songs"
+  get "/one-lyric", controller: "lyrics", action: "one_song"
+  get "/second-lyric", controller: "lyrics", action: "second_song"
+  get "/all-lyrics", controller: "lyrics", action: "all_songs"
   get "/lyrics" => "lyrics#index"
+  get "/lyric-by-id", controller: "lyrics", action: "search_id"
+  get "/lyrics/:id.json" => "lyrics#search_id", as: :search_lyric
   post "/new-lyrics" => "lyrics#create"
+  patch "/lyrics/:id.json" => "lyrics#update", as: :update_lyric
 end
