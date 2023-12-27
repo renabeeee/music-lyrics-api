c10802c9e2260c4885014b96569c16372331df1b
Serena Green <serenafgreen@gmail.com>
2023-12-26 22:53:04 -0800

    separates each component into it's own file

---
+++ music-lyrics-app-frontend/src/LyricsIndex.jsx
@@ -0,0 +1,21 @@
+export function LyricsIndex(props) {
+  console.log(props);
+  return (
+    <div id="lyrics-index" className="content-section">
+      <h1>All tracks</h1>
+
+      {props.lyrics.map((lyric) => (
+        <div key={lyric.id} className="lyrics">
+          <h2>{lyric.title}</h2>
+          <img
+            src={lyric.image_url}
+            alt={lyric.title}
+            className="lyrics-image"
+          />
+          <p>Artist: {lyric.artist}</p>
+          <button className="info-button">See more info</button>
+        </div>
+      ))}
+    </div>
+  );
+}
