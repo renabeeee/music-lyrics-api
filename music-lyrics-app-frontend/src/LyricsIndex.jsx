920d34297e12c14dafedbfc85cfd4db57d66e617
Serena Green <serenafgreen@gmail.com>
2023-12-28 23:47:27 -0800

    adds a sign up form

--- music-lyrics-app-frontend/src/LyricsIndex.jsx
+++ music-lyrics-app-frontend/src/LyricsIndex.jsx
@@ -1,26 +1,41 @@
 export function LyricsIndex(props) {
   console.log(props);
+
   return (
     <div id="lyrics-index" className="content-section">
       <h1>All tracks</h1>
 
-      {props.lyrics.map((lyric) => (
-        <div key={lyric.id} className="lyrics">
-          <h2>{lyric.title}</h2>
-          <img
-            src={lyric.image_url}
-            alt={lyric.title}
-            className="lyrics-image"
-          />
-          <p>Artist: {lyric.artist}</p>
-          <button
-            onClick={() => props.onShowLyric(lyric)}
-            className="info-button"
-          >
-            See more info
-          </button>
-        </div>
-      ))}
+      <div className="lyrics-container">
+        {props.lyrics.map((lyric) => (
+          <div key={lyric.id} className="lyrics">
+            {/* Moved card section within lyrics container */}
+            <div className="card-section">
+              <div className="card mb-3">
+                <img src={lyric.image_url} className="card-img-top" alt="..." />
+
+                <div className="card-body">
+                  <h5 className="card-title">{lyric.artist}</h5>
+
+                  <p className="card-text">
+                    {" "}
+                    <p>Title: {lyric.title}</p>
+                    <p>Duration: {lyric.duration}</p>
+                    {/* This is a wider card with supporting text below as a natural
+                    lead-in to additional content. This content is a little bit
+                    longer. */}
+                  </p>
+                </div>
+              </div>
+              <button
+                onClick={() => props.onShowLyric(lyric)}
+                className="info-button"
+              >
+                See more info
+              </button>
+            </div>
+          </div>
+        ))}
+      </div>
     </div>
   );
 }
---
+++ music-lyrics-app-frontend/src/Signup.jsx
@@ -0,0 +1,22 @@
+export function Signup() {
+  return (
+    <div id="signup">
+      <h1>Sign up!</h1>
+      <form>
+        <div>
+          Name: <input type="text" />
+        </div>
+        <div>
+          Email: <input type="text" />
+        </div>
+        <div>
+          Password: <input type="text" />
+        </div>
+        <div>
+          Password Confirmation: <input type="text" />
+        </div>
+        <button type="submit"> Sign up!</button>
+      </form>
+    </div>
+  );
+}
