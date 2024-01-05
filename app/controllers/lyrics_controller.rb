b5c84985e7b3419f9a5f07df74798b6ea8c7a947
“Serena” <serenafgreen@gmail.com>
2024-01-05 01:31:51 -0800

    adds edit and delete actions to frontend

--- app/controllers/lyrics_controller.rb
+++ app/controllers/lyrics_controller.rb
@@ -1,6 +1,6 @@
 class LyricsController < ApplicationController
 
-  before_action :authenticate_user, except: [:index, :show]
+  # before_action :authenticate_user, except: [:index, :show]
 
   def one_song
     lyric = Lyric.first
--- music-lyrics-app-frontend/src/LyricsShow.jsx
+++ music-lyrics-app-frontend/src/LyricsShow.jsx
@@ -1,13 +1,64 @@
 /* eslint-disable react/prop-types */
 export function LyricsShow(props) {
+  const handleSubmit = (event) => {
+    event.preventDefault();
+    const params = new FormData(event.target);
+    props.onUpdateLyric(props.lyric.id, params);
+    event.target.reset();
+  };
+
+  const handleDestroyLyric = () => {
+    props.onDestroyLyric(props.lyric);
+  };
+
   return (
     <div id="lyrics-show" className="show_text">
       <p>Lyric Information</p>
       <p>
-        {props.lyric.title} by {props.lyric.artist}
+        {props.lyric.BPM} by {props.lyric.artist}
       </p>
       <p>Duration: {props.lyric.duration} minutes</p>
       <p>BPM: {props.lyric.bpm}</p>
+
+      <div id="lyrics-edit" className="content-section">
+        <h1>Edit track</h1>
+
+        <form onSubmit={handleSubmit}>
+          <div className="form-field">
+            Title:{" "}
+            <input defaultValue={props.lyric.title} name="title" type="text" />
+          </div>
+          <div className="form-field">
+            Artist:{" "}
+            <input
+              defaultValue={props.lyric.artist}
+              name="artist"
+              type="text"
+            />
+          </div>
+          <div className="form-field">
+            Duration:{" "}
+            <input
+              defaultValue={props.lyric.duration}
+              name="duration"
+              type="text"
+            />
+          </div>
+          <div className="form-field">
+            BPM: <input defaultValue={props.lyric.bpm} name="bpm" type="text" />
+          </div>
+          <div className="form-field">
+            Image_url:{" "}
+            <input
+              defaultValue={props.lyric.image_url}
+              name="image_url"
+              type="text"
+            />
+          </div>
+          <button type="submit">Update lyric</button>
+        </form>
+        <button onClick={handleDestroyLyric}>Destroy lyric</button>
+      </div>
     </div>
   );
 }
