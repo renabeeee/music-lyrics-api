372ec32937436f84dfbc28f1e163b68ac9850735
Serena Green <serenafgreen@gmail.com>
2024-01-08 09:16:07 -0800

    adds search filter by titles for index

--- music-lyrics-app-frontend/src/LyricsIndex.jsx
+++ music-lyrics-app-frontend/src/LyricsIndex.jsx
@@ -1,41 +1,60 @@
 /* eslint-disable react/prop-types */
+import { useState } from "react";
+
 export function LyricsIndex(props) {
+  const [searchFilter, setSearchFilter] = useState("");
   console.log(props);
 
   return (
     <div id="lyrics-index" className="container text-center">
       <h1>All tracks</h1>
+      Search:
+      <input
+        type="text"
+        value={searchFilter}
+        onChange={(event) => setSearchFilter(event.target.value)}
+      />
       <div className="row">
         {/* <div className="lyrics-container"> */}
-        {props.lyrics.map((lyric) => (
-          <div key={lyric.id} className="lyrics col">
-            {" "}
-            <div className="card-section">
-              <div className="card mb-3">
-                <img src={lyric.image_url} className="card-img-top" alt="..." />
 
-                <div className="card-body">
-                  <h5 className="card-title">{lyric.artist}</h5>
+        {props.lyrics
+          .filter((lyric) =>
+            lyric.title.toLowerCase().includes(searchFilter.toLowerCase())
+          )
+          .map((lyric) => (
+            <div key={lyric.id} className="lyrics col">
+              {" "}
+              <div className="card-section">
+                <div className="card mb-3">
+                  <img
+                    src={lyric.image_url}
+                    className="card-img-top"
+                    alt="..."
+                  />
 
-                  <div className="card-text">
-                    {" "}
-                    <p>Title: {lyric.title}</p>
-                    <p>Duration: {lyric.duration}</p>
-                    {/* This is a wider card with supporting text below as a natural
+                  <div className="card-body">
+                    <h5 className="card-title">{lyric.artist}</h5>
+
+                    <div className="card-text">
+                      {" "}
+                      <p>Title: {lyric.title}</p>
+                      <p>Duration: {lyric.duration}</p>
+                      {/* This is a wider card with supporting text below as a natural
                     lead-in to additional content. This content is a little bit
                     longer. */}
+                    </div>
                   </div>
                 </div>
+
+                <button
+                  onClick={() => props.onShowLyric(lyric)}
+                  className="info-button"
+                >
+                  See more info
+                </button>
               </div>
-              <button
-                onClick={() => props.onShowLyric(lyric)}
-                className="info-button"
-              >
-                See more info
-              </button>
             </div>
-          </div>
-        ))}
+          ))}
       </div>
     </div>
   );
